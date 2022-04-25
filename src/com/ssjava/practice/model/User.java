2f180f4e202747224e2a96991ea7144ccfca7910
Sahitya <sahityai2021@gmail.com>
2022-04-25 11:53:58 +0530

    Added getters and setters for the name field

--- src/com/ssjava/practice/model/User.java
+++ src/com/ssjava/practice/model/User.java
@@ -3,4 +3,14 @@ package com.ssjava.practice.model;
 public class User {
 
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
