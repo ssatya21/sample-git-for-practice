86f7db339c0a3068b4e0cb6e0fea5e32435f221d
Sahitya <sahityai2021@gmail.com>
2022-04-25 11:54:40 +0530

    Added toString

--- src/com/ssjava/practice/model/User.java
+++ src/com/ssjava/practice/model/User.java
@@ -3,7 +3,7 @@ package com.ssjava.practice.model;
 public class User {
 
 	private String name;
-
+	
 	public String getName() {
 		return name;
 	}
@@ -11,6 +11,12 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + "]";
+	}
+	
 	
 	
 }
